5f2f282b463361111cee0e91fb239c884374fab3
Fraju-pc <patrick.f.corcoran@gmail.com>
2023-06-26 14:13:31 -0400

    lab finished

---
+++ week13/src/Section.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+let date = new Date()
+ 
+            let singleUser = {
+              name: 'John',
+              time: date.toDateString(),
+            }
+            
+export default class Section extends Component {
+    
+    render(){
+        return(
+            <>
+           <section className="welcome">
+            <h1>Welcome, {singleUser.name}!</h1>
+            <p>Today is {singleUser.time}</p>
+            </section>
+  
+            </>
+        )
+    }
+}
